b28487fc043b6cc1c9bf5d68b901d00e0c4f4240
Frizoid <45901008+Frizoid@users.noreply.github.com>
2024-04-12 21:08:44 +0300

    Add files via upload

---
+++ Christ tr toy V2/V2_3/V2_3.ino
@@ -0,0 +1,125 @@
+#include "TinyMAX7219.h"
+TinyMAX7219<3, 0, 2> disp;  // CS, MOSI, SCK
+
+// happy new year
+const uint8_t bitmap_56x7[] PROGMEM = {
+0xA4, 0xCC, 0xA4, 0xBA, 0x25, 0x72, 0x60,
+0xAA, 0xAA, 0xA4, 0xA2, 0xA5, 0x45, 0x50,
+0xAA, 0xAA, 0x44, 0xA2, 0xA2, 0x45, 0x50,
+0xEE, 0xCC, 0x46, 0xBA, 0xA2, 0x77, 0x60,
+0xAA, 0x88, 0x47, 0xA2, 0xA2, 0x45, 0x50,
+0xAA, 0x88, 0x45, 0xA2, 0xA2, 0x45, 0x50,
+0xAA, 0x88, 0x44, 0xB9, 0x42, 0x75, 0x50
+};
+
+
+const uint8_t star1[]   PROGMEM = {0x42, 0xA5, 0x42, 0x18, 0x18, 0x42, 0xA5, 0x42};
+const uint8_t star2[]   PROGMEM = {0xA5, 0x0, 0xA5, 0x18, 0x18, 0xA5, 0x0, 0xA5};
+const uint8_t ring1[]  PROGMEM = {0x99, 0x24, 0x42, 0x99, 0x99, 0x42, 0x24, 0x99};
+const uint8_t ring2[]  PROGMEM = {0x66, 0xDB, 0xBD, 0x66, 0x66, 0xBD, 0xDB, 0x66};
+const uint8_t pacm1[]  PROGMEM = {0xAA, 0xAA, 0x55, 0x55, 0xAA, 0xAA, 0x55, 0x55};
+const uint8_t pacm2[]  PROGMEM = {0x55, 0x55, 0xAA, 0xAA, 0x55, 0x55, 0xAA, 0xAA};
+const uint8_t cub1[] PROGMEM = {0xCC, 0xCC, 0x33, 0x33, 0xCC, 0xCC, 0x33, 0x33};
+const uint8_t cub2[] PROGMEM = {0x33, 0x33, 0xCC, 0xCC, 0x33, 0x33, 0xCC, 0xCC};
+const uint8_t heart1[] PROGMEM = {0x00, 0x66, 0x99, 0x81, 0x81, 0x42, 0x24, 0x18};
+const uint8_t heart2[] PROGMEM = {0x00, 0x66, 0xff, 0xff, 0xff, 0x7e, 0x3c, 0x18};
+const uint8_t cube1[]  PROGMEM = {0xff, 0x81, 0xbd, 0xa5, 0xa5, 0xbd, 0x81, 0xff};
+const uint8_t cube2[]  PROGMEM = {0x00, 0x7e, 0x42, 0x5a, 0x5a, 0x42, 0x7e, 0x00};
+const uint8_t bit1[] PROGMEM = {0x55, 0x80, 0x29, 0x84, 0x21, 0x94, 0x01, 0xAA};
+const uint8_t bit2[] PROGMEM = {0xAA, 0x01, 0x94, 0x21, 0x84, 0x29, 0x80, 0x55};
+
+
+byte offs = 0;
+byte mode = 0;
+byte mcount = 0;
+
+void setup()
+{
+  disp.begin();
+  disp.setBright(6);
+}
+
+void loop()
+{
+  switch (mode)
+  {
+    case 0: scrollBitmap(bitmap_40x7, 5, 7, 1);break;
+    case 1: switchBitmap(star1, star2); break; 
+    case 2: switchBitmap(pacm1, pacm2); break;
+    case 3: switchBitmap(cub1, cub2); break;//blinkBitmap(heart2); break;
+    case 4: switchBitmap(cube1, cube2); break;
+    case 5: switchBitmap(bit1, bit2); break;
+    case 6: switchBitmap(ring1, ring2); break;
+    case 7: switchBitmap(heart1, heart2); break;
+    case 8: scrollBitmap(bitmap_40x7, 5, 7, 1);break;
+  }
+  offs++;
+  if (++mcount > 41)
+  {
+    offs = mcount = 0;
+    mode++;  // mode = rnd();
+    if (mode > 8) mode = 0;
+    disp.clear();
+  }
+  delay(300);
+}
+
+void blinkBitmap(const uint8_t* bmap)
+{
+  static bool blink;
+  blink = !blink;
+  if (blink) drawBitmap(bmap);
+  else disp.clear();
+}
+
+void switchBitmap(const uint8_t* bmap1, const uint8_t* bmap2)
+{
+  static bool blink;
+  blink = !blink;
+  if (blink) drawBitmap(bmap1);
+  else drawBitmap(bmap2);
+}
+
+void drawBitmap(const uint8_t* bmap)
+{
+  for (byte i = 0; i < 8; i++)
+  {
+    disp.buf[i] = pgm_read_byte(&bmap[i]);
+  }
+  disp.update();
+}
+
+void scrollBitmap(const uint8_t* bmap, byte w, byte h, byte p)
+{
+  if (offs > (w << 3)) offs = 0;
+  for (byte i = 0; i < h; i++)
+  {
+    byte num = (offs >> 3) + w * i;
+    byte left = offs & 7;
+    byte val1 = pgm_read_byte(&bmap[num - 1]);
+    byte val2 = pgm_read_byte(&bmap[num]);
+    if (offs < 8) val1 = val2, val1 = 0;
+    if (left == 0) disp.buf[i + p] = val1;
+    else disp.buf[i + p] = ((val1 << left) | (val2 >> (8 - left)));
+  }
+  disp.update();
+}
+
+void snow()
+{
+  for (byte i = 0; i < 7; i++)
+  {
+    disp.buf[7 - i] = disp.buf[7 - i - 1];
+    if (rnd() < 2) disp.buf[7 - i] >>= 1;
+    if (rnd() > 5) disp.buf[7 - i] <<= 1;
+  }
+  disp.buf[0] = bit(rnd());
+  disp.update();
+}
+
+byte rnd()
+{
+  static uint16_t seed;
+  seed = (seed * 2053ul) + 13849;
+  return (seed >> 8) & 7;
+}
